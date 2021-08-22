09c548532eddecd62dc97e6549b6816ad6e52193
William Wang <zeweiwang@outlook.com>
2021-08-22 17:11:59 +0800

    scripts: use Verilator v4.204

--- install-verilator.sh
+++ install-verilator.sh
@@ -7,10 +7,13 @@ unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
 unset VERILATOR_ROOT  # For bash
 cd verilator
 git pull        # Make sure git repository is up-to-date
-git tag         # See what versions exist
+# git tag         # See what versions exist
 #git checkout master      # Use development branch (e.g. recent bug fixes)
 #git checkout stable      # Use most recent stable release
 #git checkout v{version}  # Switch to specified release version
+
+# XiangShan uses Verilator v4.204
+git checkout v4.204
 
 autoconf        # Create ./configure script
 ./configure     # Configure and create Makefile
