b39f2f6aebd98ad928bd6db0d614f419f15400c1
William Wang <zeweiwang@outlook.com>
2021-08-22 20:37:37 +0800

    scripts: install verilator manually (#4)
    * scripts: install verilator manually
    * scripts: add verilator dependency
    * scripts: use verilator v4.204
    * scripts: limit make verilator thread number
    * scripts: fix verilator dependency

---
+++ install-verilator.sh
@@ -0,0 +1,29 @@
+# https://verilator.org/guide/latest/install.html
+
+sudo apt-get install git perl python3 make autoconf g++ flex bison ccache
+sudo apt-get install libgoogle-perftools-dev numactl perl-doc
+sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+
+git clone https://github.com/verilator/verilator
+
+# Every time you need to build:
+unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
+unset VERILATOR_ROOT  # For bash
+cd verilator
+git pull        # Make sure git repository is up-to-date
+# git tag         # See what versions exist
+#git checkout master      # Use development branch (e.g. recent bug fixes)
+#git checkout stable      # Use most recent stable release
+#git checkout v{version}  # Switch to specified release version
+
+# XiangShan uses Verilator v4.204
+git checkout v4.204
+
+autoconf        # Create ./configure script
+./configure     # Configure and create Makefile
+make -j8        # Build Verilator itself
+sudo make install
+
+verilator --version
