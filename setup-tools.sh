d1b2f60667d8b5c1b8a44baa5b2a90df821c0512
William Wang <zeweiwang@outlook.com>
2021-08-22 16:49:56 +0800

    scripts: add verilator dependency

--- setup-tools.sh
+++ setup-tools.sh
@@ -2,7 +2,7 @@
 # tested on ubuntu 20.04 Docker image
 
 apt update
-apt install proxychains4 shadowsocks-libev vim wget git tmux make gcc time curl libreadline6-dev libsdl2-dev gcc-riscv64-linux-gnu openjdk-11-jre zlib1g-dev device-tree-compiler
+apt install proxychains4 shadowsocks-libev vim wget git tmux make gcc time curl libreadline6-dev libsdl2-dev gcc-riscv64-linux-gnu openjdk-11-jre zlib1g-dev device-tree-compiler flex autoconf bison
 sh -c "curl -L https://github.com/com-lihaoyi/mill/releases/download/0.9.8/0.9.8 > /usr/local/bin/mill && chmod +x /usr/local/bin/mill"
 
 # We need to use Verilator 4.204+, so we install Verilator manually
