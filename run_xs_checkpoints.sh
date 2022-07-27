d92c2f691b79f9beb8836887919815d3fe958895
Yinan Xu <xuyinan@ict.ac.cn>
2022-07-27 19:55:05 +0800

    fix scripts

--- run_xs_checkpoints.sh
+++ run_xs_checkpoints.sh
@@ -1,8 +1,8 @@
 set -x
 set -e
 
-python3 env-scripts/perf/xs_autorun.py                                                       \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/take_cpt                   \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/json/simpoint_summary.json \
+python3 env-scripts/perf/xs_autorun.py                                                 \
+  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt              \
+  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/simpoint_summary.json \
   --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_MICRO_2022
 
