a8a42c20ee5af14874decd67c24321397175f706
Yinan Xu <xuyinan@ict.ac.cn>
2022-07-28 16:40:56 +0800

    update report scripts

--- report_xs_checkpoints.sh
+++ report_xs_checkpoints.sh
@@ -1,5 +1,6 @@
 set -x
 set -e
 
-python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_2022 --recursive
+python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_2022 \
+  --recursive -o sjeng.csv
 
