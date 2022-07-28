aca6cfdde25e09607d159f7bf5781880eeade47b
Yinan Xu <xuyinan@ict.ac.cn>
2022-07-28 12:36:56 +0800

    udpate for pubs

--- report_xs_checkpoints.sh
+++ report_xs_checkpoints.sh
@@ -2,8 +2,7 @@ set -x
 set -e
 
 python3 env-scripts/perf/xs_autorun.py                                                 \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt              \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/simpoint_summary.json \
-  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_MICRO_2022         \
+  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gc_o2_20m/take_cpt sjeng.json   \
+  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_SJENG_2022         \
   --report -j100
 
--- run_xs_checkpoints.sh
+++ run_xs_checkpoints.sh
@@ -2,7 +2,6 @@ set -x
 set -e
 
 python3 env-scripts/perf/xs_autorun.py                                                       \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/take_cpt                   \
-  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/json/simpoint_summary.json \
-  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_NH_MICRO_2022
+  /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/take_cpt sjeng.json        \
+  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_SJENG_2022
 
