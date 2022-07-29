28b40753cc2cf982edbe7fefb506a8d2b963e8c5
Yinan Xu <xuyinan@ict.ac.cn>
2022-07-29 15:43:16 +0800

    use BASE for results

--- report_xs_checkpoints.sh
+++ report_xs_checkpoints.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
 
-python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_2022 \
+python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_BASE_2022 \
   --recursive -o sjeng_pubs_baseline.csv
 
