8909cb43e9ac2727968b8138b316bb5de35c049d
Yinan Xu <xuyinan@ict.ac.cn>
2022-07-29 15:43:47 +0800

    use AGE for results

--- report_xs_checkpoints.sh
+++ report_xs_checkpoints.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
 
-python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_2022 \
+python3 env-scripts/perf/perf.py XiangShan/SPEC06_EmuTasks_SJENG_AGE_2022 \
   --recursive -o sjeng_age_only.csv
 
--- run_xs_checkpoints.sh
+++ run_xs_checkpoints.sh
@@ -3,5 +3,5 @@ set -e
 
 python3 env-scripts/perf/xs_autorun.py                                                       \
   /nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o2_20m/take_cpt sjeng.json        \
-  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_SJENG_2022
+  --xs $XS_PROJECT_ROOT/XiangShan --threads 8 --dir SPEC06_EmuTasks_SJENG_AGE_2022
 
