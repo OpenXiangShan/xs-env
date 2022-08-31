7347922d723974076ba6bf56c77c43537da0a178
William Wang <zeweiwang@outlook.com>
2022-08-31 16:44:44 +0800

    setup.sh: use riscv64-linux-gnu- by default

--- setup.sh
+++ setup.sh
@@ -14,7 +14,8 @@ cd ${NEMU_HOME}
 make riscv64-xs-ref_defconfig
 make -j
 
+# Use riscv64-linux-gnu- toolchain by default
 cd ${AM_HOME}/apps/coremark
-make ARCH=riscv64-xs -k
+make ARCH=riscv64-xs LINUX_GNU_TOOLCHAIN=1 -k
 
 cd ${XS_PROJECT_ROOT}
