0076adb29f31aba66cf2df88823c26a96fb92d6b
Yinan Xu <xuyinan@ict.ac.cn>
2022-09-21 15:01:41 +0800

    Update install-verilator.sh

--- install-verilator.sh
+++ install-verilator.sh
@@ -9,14 +9,9 @@ sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives err
 git clone https://github.com/verilator/verilator
 
 # Every time you need to build:
-unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
 unset VERILATOR_ROOT  # For bash
 cd verilator
 git pull        # Make sure git repository is up-to-date
-# git tag         # See what versions exist
-#git checkout master      # Use development branch (e.g. recent bug fixes)
-#git checkout stable      # Use most recent stable release
-#git checkout v{version}  # Switch to specified release version
 
 # XiangShan uses Verilator v4.218
 git checkout v4.218
