6643531522d7ef751359e46046c6606b4c47cb98
William Wang <zeweiwang@outlook.com>
2023-03-07 18:06:49 +0800

    Add DRAMsim3 to xs-env (#39)

--- env.sh
+++ env.sh
@@ -4,8 +4,10 @@ export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
 export NOOP_HOME=$(pwd)/XiangShan
+export DRAMSIM3_HOME=$(pwd)/DRAMSim3
 
 echo SET XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
+echo SET NOOP_HOME \(XiangShan RTL Home\): ${NOOP_HOME}
 echo SET NEMU_HOME: ${NEMU_HOME}
 echo SET AM_HOME: ${AM_HOME}
-echo SET NOOP_HOME: ${NOOP_HOME}
+echo SET DRAMSIM3_HOME: ${DRAMSIM3_HOME}
