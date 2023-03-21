d156247e8cd8c1a0bb7477c72bd7874f8c0cc591
William Wang <zeweiwang@outlook.com>
2023-03-21 14:02:01 +0800

    Fix DRAMsim3 path in env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
 export NOOP_HOME=$(pwd)/XiangShan
-export DRAMSIM3_HOME=$(pwd)/DRAMSim3
+export DRAMSIM3_HOME=$(pwd)/DRAMsim3
 
 echo SET XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
 echo SET NOOP_HOME \(XiangShan RTL Home\): ${NOOP_HOME}
