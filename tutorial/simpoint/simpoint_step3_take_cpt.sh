28f51471add0951adc9c5727f08460dd9f31eea8
good-circle <fenghaoyuan19@mails.ucas.ac.cn>
2023-03-23 20:43:11 +0800

    Update simpoint scripts

--- tutorial/simpoint/simpoint_step3_take_cpt.sh
+++ tutorial/simpoint/simpoint_step3_take_cpt.sh
@@ -5,8 +5,8 @@ rm -rf $NEMU_HOME/tutorial_simpoint/stream/take_cpt
 ./build/riscv64-nemu-interpreter \
     -b \
     -D tutorial_simpoint \
-    -w stream \
     -C take_cpt \
+    -w stream \
     -S ./tutorial_simpoint/cluster \
     --checkpoint-interval 50000000 \
     ../tutorial/bin/stream-0xa0000.bin
--- tutorial/simpoint/uniform_step2_run_nemu.sh
+++ tutorial/simpoint/uniform_step2_run_nemu.sh
@@ -1,6 +1,7 @@
 cd $NEMU_HOME
 
-./build/riscv64-nemu-interpreter -b \
+./build/riscv64-nemu-interpreter \
+    -b \
     --restore \
     -I 1000000 \
     `find tutorial_uniform/try/linux/0/ -type f -name "*.gz" | tail -1`
