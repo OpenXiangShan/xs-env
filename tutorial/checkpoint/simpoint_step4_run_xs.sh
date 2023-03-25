cefe9dd3595810b39af0aa9fcb55179615d765ae
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2023-03-25 21:53:29 +0800

    Update simpoint_step4_run_xs.sh

--- tutorial/checkpoint/simpoint_step4_run_xs.sh
+++ tutorial/checkpoint/simpoint_step4_run_xs.sh
@@ -1,4 +1,5 @@
 $XS_PROJECT_ROOT/tutorial/emu \
     -i `find $NEMU_HOME/tutorial_simpoint/ -type f -name "*.gz" | tail -1` \
     --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so \
-    --max-cycles=100000
+    --max-cycles=100000 \
+    2>simpoint.err
