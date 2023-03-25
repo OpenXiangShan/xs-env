b4be4f8e00132476ac6147029a0bd7de63cf6112
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2023-03-25 19:14:52 +0800

    update TLLog analyze

--- tutorial/analyze_db.sh
+++ tutorial/analyze_db.sh
@@ -1,4 +1,3 @@
-printf "%d" 0x80000dc0 | xargs -I Eaddr \
-	sqlite3 $NOOP_HOME/build/*.db \
+printf "%d" 0x80000dc0 | xargs -I Eaddr sqlite3 $NOOP_HOME/build/*.db \
 	"select * from TLLOG where ADDRESS=Eaddr" | \
 	sh $NOOP_HOME/scripts/utils/parseTLLog.sh
