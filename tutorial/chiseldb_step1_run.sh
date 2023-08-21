1ba55d7c2fa70c4d2ba238a99554a2ec5f72bdfb
ceba <ceba_robot@outlook.com>
2023-08-21 11:10:42 +0800

    fix: strange space in chiseldb_step1_run.sh

--- tutorial/chiseldb_step1_run.sh
+++ tutorial/chiseldb_step1_run.sh
@@ -1,3 +1,3 @@
 set -v
 
-./emu-cc-err -i $NOOP_HOME/ready-to-run/linux.bin --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so --dump-db 2>linux.err
+./emu-cc-err -i $NOOP_HOME/ready-to-run/linux.bin --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so --dump-db 2>linux.err
