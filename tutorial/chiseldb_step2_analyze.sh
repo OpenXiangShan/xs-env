1b4c136acfc0800dbb624dbdc502dfaa47c32852
ceba <ceba_robot@outlook.com>
2023-08-21 11:26:02 +0800

    fix: problem of  chiseldb_step2_analyze.sh

--- tutorial/chiseldb_step2_analyze.sh
+++ tutorial/chiseldb_step2_analyze.sh
@@ -1,3 +1,3 @@
 set -v
 
-xargs -I Eaddr sqlite3 $NOOP_HOME/build/*.db "select * from TLLOG where ADDRESS=Eaddr" | sh $NOOP_HOME/scripts/utils/parseTLLog.sh
+sqlite3 $NOOP_HOME/build/*.db "select * from TLLOG where ADDRESS=0x80000dc0" | sh $NOOP_HOME/scripts/utils/parseTLLog.sh
