f5b1fe944df1c94a90f5fe58242a0ab9ea0f5657
Xu Zefan <xuzefan@mail.ustc.edu.cn>
2023-08-21 09:54:28 +0800

    fix & feat: update some tutorial scripts

--- tutorial/analyze_db.sh
+++
@@ -1,3 +0,0 @@
-printf "%d" 0x80000dc0 | xargs -I Eaddr sqlite3 $NOOP_HOME/build/*.db \
-	"select * from TLLOG where ADDRESS=Eaddr" | \
-	sh $NOOP_HOME/scripts/utils/parseTLLog.sh
---
+++ tutorial/run-emu-perf.sh
@@ -0,0 +1,3 @@
+set -v
+
+./emu -i hello.bin --diff $NEMU_HOME/build/riscv64-nemu-interpreter-so 2>perf.err
