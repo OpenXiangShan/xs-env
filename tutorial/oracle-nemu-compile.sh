098cd4754f2eea6bad99c778e2d9b56e175691f6
xuzefan <ceba_robot@outlook.com>
2023-10-25 18:32:10 +0800

    Remove Oracle related

--- tutorial/oracle-nemu-compile.sh
+++
@@ -1,7 +0,0 @@
-export NEMU_HOME=${XS_PROJECT_ROOT}/NEMU-ahead
-cd ${NEMU_HOME}
-make riscv64-xs-ahead-ref_defconfig
-make -j16
-export AHEAD_HOME=${XS_PROJECT_ROOT}/NEMU-ahead
-# redirect NEMU_HOME to difftest version
-export NEMU_HOME=${XS_PROJECT_ROOT}/NEMU
--- tutorial/oracle-rtl-compile.sh
+++
@@ -1,2 +0,0 @@
-cd ${NOOP_HOME}
-make emu EMU_THREADS=8 EMU_TRACE=1 WITH_LIGHTQS=1 CONFIG=MinimalConfig  -j8
