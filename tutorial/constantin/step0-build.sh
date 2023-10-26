38429662238d51a4a5dda4494f1ac1dd15781cb3
xuzefan <ceba_robot@outlook.com>
2023-10-26 14:23:33 +0800

    Update constantin scripts

--- tutorial/constantin/step0-build.sh
+++ tutorial/constantin/step0-build.sh
@@ -1,2 +1,13 @@
+set -v -e
+
+cd ${NOOP_HOME}/utility
+git apply ${XS_PROJECT_ROOT}/tutorial/constantin/utility.patch
+
 cd ${NOOP_HOME}
-make emu CONFIG=TutorialConfig EMU_THREADS=1 EMU_TRACE=1 WITH_CONSTANTIN=1 -j8
+make clean
+make emu EMU_THREADS=4 EMU_TRACE=1 WITH_CONSTANTIN=1 MFC=1 -j16
+
+cp ${NOOP_HOME}/build/emu ${XS_PROJECT_ROOT}/tutorial/emu-constantin
+
+cd ${NOOP_HOME}/utility
+git apply -R ${XS_PROJECT_ROOT}/tutorial/constantin/utility.patch
