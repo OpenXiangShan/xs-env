f020b57c5d5b89a921e14f8f86c3abb201bd5f63
xuzefan <ceba_robot@outlook.com>
2023-10-28 14:25:21 +0800

    Checkpoint: clean before make

--- tutorial/checkpoint/simpoint-step4-run-xs.sh
+++ tutorial/checkpoint/simpoint-step4-run-xs.sh
@@ -1,3 +1,5 @@
+set -v
+
 $XS_PROJECT_ROOT/tutorial/emu \
     -i `find $NEMU_HOME/tutorial_simpoint/ -type f -name "*_.gz" | tail -1` \
     --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so \
