49db17368a8517a426e5b9987ad1be4f017e3de1
xuzefan <ceba_robot@outlook.com>
2023-10-28 23:08:08 +0800

    tutorial: fix build am script

--- tutorial/build-am.sh
+++ tutorial/build-am.sh
@@ -3,7 +3,6 @@ set -v
 cd $AM_HOME
 make clean
 cd apps/coremark
-make ARCH=riscv64-xs ITERATIONS=1
+make ARCH=riscv64-xs ITERATIONS=1 LINUX_GNU_TOOLCHAIN=1
 echo -e '\nOutput workload:'
 ls build
-    
