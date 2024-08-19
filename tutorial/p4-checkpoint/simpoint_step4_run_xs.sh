faf1cc32a1e927ed0550af432eab4679c2a61b64
lewislzh <liuzehao19@mails.ucas.ac.cn>
2024-08-19 19:40:17 +0800

    fix simpoint_run_xs

--- tutorial/p4-checkpoint/simpoint_step4_run_xs.sh
+++ tutorial/p4-checkpoint/simpoint_step4_run_xs.sh
@@ -3,5 +3,5 @@
 
 source simpoint_env.sh
 set -x
-$NEMU -b `find $RESULT/checkpoint/stream -type f -name "*_.gz" | tail -1` --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so --max-cycles=50000 2>simpoint.err
+./emu -i `find $RESULT/checkpoint/stream -type f -name "*_.gz" | tail -1` --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so --max-cycles=50000 2>simpoint.err
 
