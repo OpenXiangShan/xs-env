821fc6e129af7fcdfe63c748168a8bb3a675275f
Yanqin Li <maxpicca@qq.com>
2024-08-19 17:04:12 +0800

    tutorial: update constantin scripts

--- tutorial/p7-constantin/step0-build.sh
+++ tutorial/p7-constantin/step0-build.sh
@@ -14,7 +14,7 @@ cst=${XS_PROJECT_ROOT}/tutorial/${dir}/my_constantin.txt
 cat $cst | make emu EMU_THREADS=4 WITH_CONSTANTIN=1 -j200 \
 PGO_WORKLOAD=$wld PGO_MAX_CYCLE=100000 PGO_EMU_ARGS=--no-diff LLVM_PROFDATA=llvm-profdata
 
-cp ${NOOP_HOME}/build/emu ${XS_PROJECT_ROOT}/tutorial/${dir}/emu-constantin
+cp ${NOOP_HOME}/build/emu ${XS_PROJECT_ROOT}/tutorial/${dir}/emu
 
 cd ${NOOP_HOME}/utility
 git apply -R ${XS_PROJECT_ROOT}/tutorial/${dir}/utility.patch
