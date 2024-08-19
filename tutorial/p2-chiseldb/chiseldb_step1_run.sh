06d187c4be06eccb0144a32b7a0ca1c30240039a
Ma-YX <mayuexiao19@mails.ucas.ac.cn>
2024-08-19 17:50:33 +0800

    update p2 scripts

--- tutorial/p2-chiseldb/chiseldb_step1_run.sh
+++ tutorial/p2-chiseldb/chiseldb_step1_run.sh
@@ -1,1 +1,4 @@
+rm -rf $NOOP_HOME/build
+mkdir $NOOP_HOME/build
+
 ./emu-cdb-err -i $NOOP_HOME/ready-to-run/linux.bin --diff $NOOP_HOME/ready-to-run/riscv64-nemu-interpreter-so --dump-db 2>linux.err
--- tutorial/p2-chiseldb/chiseldb_step2_analyze.sh
+++ tutorial/p2-chiseldb/chiseldb_step2_analyze.sh
@@ -1,1 +1,2 @@
-sqlite3 $NOOP_HOME/build/*.db "select * from TLLog where ADDRESS=0x800419c0" | sh $NOOP_HOME/scripts/cache/convert_tllog.sh
+DB=$(ls -t $NOOP_HOME/build/*db | head -n 1)
+sqlite3 $DB "select * from TLLog where ADDRESS=0x800419c0" | sh $NOOP_HOME/scripts/cache/convert_tllog.sh
