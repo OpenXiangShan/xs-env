abe49fde33b918e86ac34ad63e94c8e6441a6b97
Ma-YX <71326427+Ma-YX@users.noreply.github.com>
2024-08-19 14:18:10 +0800

    Update env.sh, add TLT_HOME

--- env.sh
+++ env.sh
@@ -5,9 +5,11 @@ export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
 export NOOP_HOME=$(pwd)/XiangShan
 export DRAMSIM3_HOME=$(pwd)/DRAMsim3
+export TLT_HOME=$(pwd)/tl-test-new
 
 echo SET XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
 echo SET NOOP_HOME \(XiangShan RTL Home\): ${NOOP_HOME}
 echo SET NEMU_HOME: ${NEMU_HOME}
 echo SET AM_HOME: ${AM_HOME}
 echo SET DRAMSIM3_HOME: ${DRAMSIM3_HOME}
+echo SET TLT_HOME: ${TLT_HOME}
