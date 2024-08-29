eea41af86385162afb40ad718421232ab9f0df9d
Yangyu Chen <cyy@cyyself.name>
2024-08-29 11:23:42 +0800

    tutorial: change gem5 data path
    Signed-off-by: Yangyu Chen <cyy@cyyself.name>

--- tutorial/p5-xs-gem5/5-gem5_spec06_score.sh
+++ tutorial/p5-xs-gem5/5-gem5_spec06_score.sh
@@ -5,9 +5,9 @@ pushd ../../ >/dev/null && source env.sh >/dev/null && popd >/dev/null
 pushd gem5_data_proc && \
 mkdir -p results && \
 export PYTHONPATH=`pwd` && \
-python3 batch.py -s /home/share/xs-model-l1bank -o gem5-score-example.csv && \
+python3 batch.py -s ../data/xs-model-l1bank -o gem5-score-example.csv && \
 python3 simpoint_cpt/compute_weighted.py \
     -r gem5-score-example.csv \
-    -j /home/share/xs-model-l1bank/cluster-0-0.json \
+    -j ../data/xs-model-l1bank/cluster-0-0.json \
     --score score.csv && \
 popd
