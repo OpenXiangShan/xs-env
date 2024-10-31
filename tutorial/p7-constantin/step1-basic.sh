62f4c6e4777ee28cb43c64f1655040acd9b3536f
Yanqin Li <maxpicca@qq.com>
2024-11-01 00:56:30 +0800

    tutorial: add manual input in constantin

--- tutorial/p7-constantin/step1-basic.sh
+++ tutorial/p7-constantin/step1-basic.sh
@@ -3,4 +3,6 @@ dir="p7-constantin"
 
 wld=${XS_PROJECT_ROOT}/tutorial/${dir}/maprobe-riscv64-xs.bin
 cst=${XS_PROJECT_ROOT}/tutorial/${dir}/my_constantin.txt
-cat $cst | ./emu -i $wld -I 1000 --diff ${NOOP_HOME}/ready-to-run/riscv64-nemu-interpreter-so 2> ${XS_PROJECT_ROOT}/tutorial/${dir}/constantin.err
+### If you don't want to input it manually, you can replace the next line with this line
+# cat $cst | ./emu -i $wld -I 1000 --diff ${NOOP_HOME}/ready-to-run/riscv64-nemu-interpreter-so 2> ${XS_PROJECT_ROOT}/tutorial/${dir}/constantin.err
+./emu -i $wld -I 1000 --diff ${NOOP_HOME}/ready-to-run/riscv64-nemu-interpreter-so 2> ${XS_PROJECT_ROOT}/tutorial/${dir}/constantin.err
