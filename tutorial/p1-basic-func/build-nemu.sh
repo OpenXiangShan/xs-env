1e7013e4f24d9a87fa0d9b288a6ccf68c0365eee
Yanqin Li <maxpicca@qq.com>
2024-11-01 01:40:23 +0800

    tutorial: add a prompt for the first run of NEMU

--- tutorial/p1-basic-func/build-nemu.sh
+++ tutorial/p1-basic-func/build-nemu.sh
@@ -1,6 +1,10 @@
 set -v
 
 cd $NEMU_HOME
+### If you are building NEMU for the first time,
+### you will need to run the following command to configure it.
+### Here we have configured it in advance.
+# make menuconfig
 make clean
 make riscv64-xs_defconfig
 make -j
