52b8627a58eed7c7be4247f4f3f3d3e2a4dc0c7a
qiuzeyuan <qiu_zeyuan@126.com>
2025-02-20 18:07:10 +0800

    env: update environment variable paths to use realpath
    Update the environment variable path using $(dirname $(realpath $0)) so that source env.sh can be "source" in any folder.

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 # This script will setup XiangShan environment variables
 
-export XS_PROJECT_ROOT=$(pwd)
-export NEMU_HOME=$(pwd)/NEMU
-export AM_HOME=$(pwd)/nexus-am
-export NOOP_HOME=$(pwd)/XiangShan
-export DRAMSIM3_HOME=$(pwd)/DRAMsim3
+export XS_PROJECT_ROOT=$(dirname $(realpath $0))
+export NEMU_HOME=$XS_PROJECT_ROOT/NEMU
+export AM_HOME=$XS_PROJECT_ROOT/nexus-am
+export NOOP_HOME=$XS_PROJECT_ROOT/XiangShan
+export DRAMSIM3_HOME=$XS_PROJECT_ROOT/DRAMsim3
 
 echo SET XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
 echo SET NOOP_HOME \(XiangShan RTL Home\): ${NOOP_HOME}
