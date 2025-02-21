3fe89a270d6971da540cbf56b9bd1f89f49d0959
Ma-YX <mayuexiao19@mails.ucas.ac.cn>
2025-02-21 15:15:25 +0800

    docs(tutorial): modify scripts of fun part

--- tutorial/p1-basic-func/run-nemu.sh
+++ tutorial/p1-basic-func/run-nemu.sh
@@ -1,4 +1,4 @@
 set -v
 
 cd $NEMU_HOME
-./build/riscv64-nemu-interpreter -b $AM_HOME/apps/coremark/build/coremark-riscv64-xs.bin
+./build/riscv64-nemu-interpreter -b $AM_HOME/apps/coremark/build/coremark-1-iteration-riscv64-xs.bin
--- tutorial/p2-chiseldb/chiseldb_step2_analyze.sh
+++ tutorial/p2-chiseldb/chiseldb_step2_analyze.sh
@@ -1,2 +1,2 @@
 DB=$(ls -t $NOOP_HOME/build/*db | head -n 1)
-sqlite3 $DB "select * from TLLog where ADDRESS=0x800419c0" | sh $NOOP_HOME/scripts/cache/convert_tllog.sh
+sqlite3 $DB "select * from TLLog where ADDRESS=0x80040580" | sh $NOOP_HOME/scripts/cache/convert_tllog.sh
