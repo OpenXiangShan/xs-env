db4101852de32d07193ef53a0bd4b17cfa17a9b3
Ma-YX <71326427+Ma-YX@users.noreply.github.com>
2025-02-25 18:25:22 +0800

    docs(run-emu-dumpwave): change wave-form cycles

--- tutorial/p1-basic-func/run-emu-dumpwave.sh
+++ tutorial/p1-basic-func/run-emu-dumpwave.sh
@@ -3,4 +3,4 @@ rm $NOOP_HOME/build/*.vcd
 
 set -v
 
-./emu-bug -i $AM_HOME/apps/coremark/build/coremark-1-iteration-riscv64-xs.bin --diff $NEMU_HOME/build/riscv64-nemu-interpreter-so -b 13000 -e 14000 --dump-wave
+./emu-bug -i $AM_HOME/apps/coremark/build/coremark-1-iteration-riscv64-xs.bin --diff $NEMU_HOME/build/riscv64-nemu-interpreter-so -b 9000 -e 11000 --dump-wave
