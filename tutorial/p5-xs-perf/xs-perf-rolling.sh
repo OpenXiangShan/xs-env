cbd3c28d7ae575c60eed9f066457bc2e7b091cc7
Yanqin Li <maxpicca@qq.com>
2025-02-25 23:48:29 +0800

    chore: update gitignore and p5 scripts

--- tutorial/p5-xs-perf/xs-perf-rolling.sh
+++ tutorial/p5-xs-perf/xs-perf-rolling.sh
@@ -4,5 +4,3 @@ dir="p5-xs-perf"
 cd ${NOOP_HOME}/scripts/rolling
 python3 rollingplot.py ${XS_PROJECT_ROOT}/tutorial/${dir}/xs-perf-rolling.db ipc
 ls ${NOOP_HOME}/scripts/rolling/results
-
-cp ${NOOP_HOME}/scripts/top-down/results/perf.png ./xsrolling.png
