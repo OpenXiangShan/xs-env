d85795349c7a0f8ef4efa272f5dfafd549f52d53
xu_zh <ngc7331@outlook.com>
2025-05-19 10:39:01 +0800

    feat(nix): add nix devshell support (#60)
    Basically, just do whatever `setup-tools.sh` does, using nix.
    
    We can run `nix develop` to enter devshells.
    
    Also, we can use [nix-direnv](https://github.com/nix-community/nix-direnv) and run `direnv allow .` to let direnv auto management.
    
    I'll add docs to XiangShan-doc if needed.
    
    Showcase using direnv:
    ```bash
    $ direnv allow workspace/xs-env # this will be needed only the first time
    
    $ cd workspace/xs-env/XiangShan # direnv will build & activate devshell each time you enter xs-env and subdirectories
    direnv: loading ~/workspace/xs-env/.envrc
    direnv: using flake
    === Welcome to XiangShan devshell! ===
    Version info:
    - Verilator 5.028 2024-08-21 rev v5.028
    - Mill Build Tool version 0.12.3
    - gcc (GCC) 14.2.1 20250322
    - riscv64-unknown-linux-gnu-gcc (GCC) 14.2.1 20250322
    - openjdk version "21.0.7" 2025-04-15
    You can press Ctrl + D to exit devshell.
    direnv: export +AR +AS +CC +CLASSPATH +CONFIG_SHELL +CXX +DETERMINISTIC_BUILD +HOST_PATH +IN_NIX_SHELL +JAVA_HOME +LD +NIX_BINTOOLS +NIX_BINTOOLS_WRAPPER_TARGET_HOST_riscv64_unknown_linux_gnu +NIX_BINTOOLS_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu +NIX_BUILD_CORES +NIX_BUILD_TOP +NIX_CC +NIX_CC_WRAPPER_TARGET_HOST_riscv64_unknown_linux_gnu +NIX_CC_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu +NIX_CFLAGS_COMPILE +NIX_ENFORCE_NO_NATIVE +NIX_HARDENING_ENABLE +NIX_LDFLAGS +NIX_STORE +NM +OBJCOPY +OBJDUMP +PYTHONHASHSEED +PYTHONNOUSERSITE +PYTHONPATH +RANLIB +READELF +SDL2_PATH +SIZE +SOURCE_DATE_EPOCH +STRINGS +STRIP +TEMP +TEMPDIR +TMP +TMPDIR +_PYTHON_HOST_PLATFORM +_PYTHON_SYSCONFIGDATA_NAME +__structuredAttrs +buildInputs +buildPhase +builder +cmakeFlags +configureFlags +depsBuildBuild +depsBuildBuildPropagated +depsBuildTarget +depsBuildTargetPropagated +depsHostHost +depsHostHostPropagated +depsTargetTarget +depsTargetTargetPropagated +doCheck +doInstallCheck +dontAddDisableDepTrack +mesonFlags +name +nativeBuildInputs +out +outputs +patches +phases +preferLocalBuild +propagatedBuildInputs +propagatedNativeBuildInputs +shell +shellHook +stdenv +strictDeps +system ~PATH ~XDG_DATA_DIRS
    
    $
    ```

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Nix devshells for XiangShan";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # === tool ===
+        wget
+        git
+        tmux
+        curl
+        time
+
+        # === runtime ===
+        python3
+        python3Packages.psutil # for XiangShan/scripts/xiangshan.py
+        openjdk
+
+        # === toolchain ===
+        gcc # host toolchain
+        pkgsCross.riscv64.buildPackages.gcc # riscv64-unknown-elf-xxx toolchain
+        llvm # for pgo
+        clang
+        gnumake # make
+        dtc # device tree compiler
+        flex
+        autoconf
+        bison
+        # override mill & verilator to use our version
+        (mill.overrideAttrs (finalAttrs: previousAttrs: {
+          version = "0.12.3";
+          src = fetchurl {
+            url = "https://github.com/com-lihaoyi/mill/releases/download/${finalAttrs.version}/${finalAttrs.version}-assembly";
+            hash = "sha256-hqzAuYadCciYPs/b6zloLUfrWF4rRtlBSMxSj7tLg7g=";
+          };
+        }))
+        (verilator.overrideAttrs (finalAttrs: previousAttrs: {
+          version = "5.028";
+          VERILATOR_SRC_VERSION = "v${finalAttrs.version}";
+          src = fetchFromGitHub {
+            owner = "verilator";
+            repo = "verilator";
+            rev = "v${finalAttrs.version}";
+            hash = "sha256-YgK60fAYG5575uiWmbCODqNZMbRfFdOVcJXz5h5TLuE=";
+          };
+          doCheck = false;
+        }))
+
+        # === debug ===
+        gtkwave
+
+        # === lib ===
+        readline
+        SDL2
+        zlib
+        zstd
+        sqlite
+      ];
+      shellHook = ''
+        echo "=== Welcome to XiangShan devshell! ==="
+        echo "Version info:"
+        echo "- $(verilator --version)"
+        echo "- $(mill --version | head -n 1)"
+        echo "- $(gcc --version | head -n 1)"
+        echo "- $(riscv64-unknown-linux-gnu-gcc --version | head -n 1)"
+        echo "- $(java -version 2>&1 | head -n 1)"
+        echo "You can press Ctrl + D to exit devshell."
+      '';
+    };
+  };
+}
