f6cdcdf0d63300dca88309a24e0865952b82309a
Haojin Tang <tanghaojin@outlook.com>
2025-06-19 12:41:24 +0800

    fix(tutorial): fix topdown issue

--- tutorial/p5-xs-perf/rtl-top-down.sh
+++ tutorial/p5-xs-perf/rtl-top-down.sh
@@ -1,5 +1,6 @@
 set -v -e
 
 # ~22s
-cd ${NOOP_HOME}/scripts/top-down && python3 top_down.py -s /opt/SPEC06_EmuTasks_topdown -j /opt/SPEC06_EmuTasks_topdown.json
+P5_DIR=$XS_PROJECT_ROOT/tutorial/p5-xs-perf
+cd ${NOOP_HOME}/scripts/top-down && python3 top_down.py -s $P5_DIR/data/SPEC06_EmuTasks_topdown -j $P5_DIR/data/SPEC06_EmuTasks_topdown.json
 ls ${NOOP_HOME}/scripts/top-down/results
