2f64768225625ff4465b7e5e73f5bc054dfc3cbb
fengkehan <fengkehan@bosc.ac.cn>
2025-07-21 10:26:43 +0800

    tool: add create vivado fpga prj shell

---
+++ clean_fpgaprj.sh
@@ -0,0 +1,3 @@
+rm -rf ../XiangShan/build
+rm -rf ../XiangShan/difftest/build
+rm -rf ../XiangShan/env-scripts/xs_kmh_fpga_diff/xs_kmh
---
+++ creat_fpgaprj.sh
@@ -0,0 +1,16 @@
+XS_PROJECT_HOME=$(pwd)
+
+cd XiangShan
+export NOOP_HOME=$(pwd)
+make verilog CONFIG=FpgaDiffMinimalConfig PLDM=1 PLDM_ARGS="--difftest-config H"
+
+cd difftest
+export NOOP_HOME=$(pwd)
+make difftest_verilog PROFILE=../build/generated-src/difftest_profile.json NUM_CORES=1 CONFIG=ESBIF
+cp -r ./build ../
+
+cd ../../env-scripts/xs_kmh_fpga_diff
+make update_core_flist
+make kmh
+
+cd $XS_PROJECT_HOME
