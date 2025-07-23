41a75f4f80f06b5bb033513e27a10552a1ce0390
fengkehan <fengkehan@bosc.ac.cn>
2025-07-23 14:24:29 +0800

    shell: fix tool edit gateway interface and clean rtl

--- clean_fpgaprj.sh
+++ clean_fpgaprj.sh
@@ -1,3 +1,4 @@
-rm -rf ../XiangShan/build
-rm -rf ../XiangShan/difftest/build
-rm -rf ../XiangShan/env-scripts/xs_kmh_fpga_diff/xs_kmh
+rm -rf ./XiangShan/build
+rm -rf ./XiangShan/out
+rm -rf ./XiangShan/difftest/build
+rm -rf ./XiangShan/env-scripts/xs_kmh_fpga_diff/xs_kmh
