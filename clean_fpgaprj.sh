df6cbcebaa9244da8fc0c1bef956eb4a2ceda26e
fengkehan <fengkehan@bosc.ac.cn>
2025-07-31 15:09:37 +0800

    bump(env-scripts): fix soc clk and clean_fpgaprj.sh

--- clean_fpgaprj.sh
+++ clean_fpgaprj.sh
@@ -1,4 +1,4 @@
 rm -rf ./XiangShan/build
 rm -rf ./XiangShan/out
 rm -rf ./XiangShan/difftest/build
-rm -rf ./XiangShan/env-scripts/xs_kmh_fpga_diff/xs_kmh
+rm -rf ./env-scripts/xs_kmh_fpga_diff/xs_kmh
