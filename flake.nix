6cde67ba89b4197b8faeb54b1530562750379bb6
ngc7331 <ngc7331@outlook.com>
2025-11-01 15:20:21 +0800

    chore(nix): use nixos-25.05 stable source & update flake.lock
    this allows us better utilize nix cache (e.g. in mirrors.ustc.edu.cn)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix devshells for XiangShan";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = {nixpkgs, ...}: let
