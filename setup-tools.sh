ceebbfbe14fec669f761fd9284139715129485c1
ngc7331 <ngc7331@outlook.com>
2026-01-20 20:52:50 +0800

    fix: bolt-19

--- setup-tools.sh
+++ setup-tools.sh
@@ -35,6 +35,7 @@ apt install -y \
 # GSIM requires clang 19+
 if apt list "clang*" | grep clang-19; then
     apt install -y clang-19
+    apt install -y bolt-19 || echo "Skipping bolt-19 installation, not available in apt repos"
     for bin in $(ls /usr/bin/*-19); do
         base=$(basename $bin)
         alt=${base%-19}
@@ -45,13 +46,11 @@ else
     echo "Warning: clang-19 is not available, falling back to default clang."
     echo "This may be because you are not using the Ubuntu version we recommend."
     apt install -y clang
+    apt install -y llvm-bolt || echo "Skipping llvm-bolt installation, not available in apt repos"
 fi
 
 # openjdk has better performance with newer versions
 apt install -y openjdk-21-jre || apt install -y openjdk-11-jre
-
-# Install llvm-bolt if available
-apt install -y llvm-bolt || echo "Skipping llvm-bolt installation, not available in apt repos"
 
 sh -c "curl -L https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/1.0.4/mill-dist-1.0.4-mill.sh > /usr/local/bin/mill && chmod +x /usr/local/bin/mill"
 
