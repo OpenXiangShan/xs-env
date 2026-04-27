7bdb8ade56dd222c1ba5c957d02dc40447033d9b
ngc7331 <ngc7331@outlook.com>
2026-04-27 21:22:54 +0800

    feat: bump verilator to 5.048 & use jemalloc
    Signed-off-by: ngc7331 <ngc7331@outlook.com>

--- flake.nix
+++ flake.nix
@@ -47,17 +47,21 @@
           };
         }))
         (verilator.overrideAttrs (finalAttrs: previousAttrs: {
-          version = "5.044";
+          version = "5.048";
           VERILATOR_SRC_VERSION = "v${finalAttrs.version}";
           src = fetchFromGitHub {
             owner = "verilator";
             repo = "verilator";
             rev = "v${finalAttrs.version}";
-            hash = "sha256-z3jYNzhnZ+OocDAbmsRBWHNNPXLLvExKK1TLDi9JzPQ=";
+            hash = "sha256-xvqqgbW7L07+NBYzGN2KLhwir58ByShxo4VVPI3pgZk=";
           };
           # drop patch https://github.com/NixOS/nixpkgs/commit/06be23d66287c004cb8ef933a9ff6589276ab104
           # since it's already included in 5.044
           patches = [];
+          # use jemalloc by default
+          buildInputs = previousAttrs.buildInputs ++ [
+            jemalloc
+          ];
           doCheck = false;
         }))
         gsim.packages.${system}.default
